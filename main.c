3cec92fd913280608a61785f77934b8ca41b791f
Subhajit Patra <1souvikpa@gmail.com>
2020-09-21 21:52:14 +0530

    First commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("My name is Subhajit Patra \n");
+    printf("I read in class 9\n");
+    printf("My hobby is reading stories of Sherlock Holmes\n");
+    printf("MY favorite sport is Chess\n");
+    return 0;
+}
