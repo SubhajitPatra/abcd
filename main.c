a977ebac21e6757c5747aa1d916781924cac9ec0
Subhajit Patra <1souvikpa@gmail.com>
2020-09-21 22:22:45 +0530

    took user input

--- main.c
+++ main.c
@@ -3,9 +3,14 @@
 
 int main()
 {
+   int number;
     printf("My name is Subhajit Patra \n");
     printf("I read in class 9\n");
     printf("My hobby is reading stories of Sherlock Holmes\n");
-    printf("MY favorite sport is Chess\n");
+    printf("MY favorite sport is Chess\n\n\n");
+    printf("what is your favorite number? type the number and press enter.\n");
+    scanf("%d",&number);
+    printf("MY favorite number is %d",number);
+
     return 0;
 }
